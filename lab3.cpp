5734ca3f19eaa9de25baa940184ec4202b708f82
Christian J. Chavez <cjchavez@fit.edu.ph>
2016-09-10 11:16:14 +0800

    Initial lab3

---
+++ lab3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+const char* yo(const char* str2){
+	//return strlen(str);
+	return strcmp(str1, str2);
+}
+
+int main(){
+	char str1[100];
+	char str2[100];
+
+	
+	cout<<myStrCmp(str1,str2);
+}
+
+
